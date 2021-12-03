6ff9a5f8a4d437d5feef81020f35aa5b88cc9d9e
spooonsss <spooonsss123@gmail.com>
2021-12-03 12:16:41 -0600

    dsx sa-1 routine

--- taptap.asm
+++ taptap.asm
@@ -10,7 +10,7 @@
 ; DEFINES           ;
 ;-------------------;
 
-prot TapTapGFX
+prot gfx
 
 !CANSPINJUMPONTAP = $00 ; Change to $01 if you want to disable this.
 
@@ -1343,91 +1343,156 @@ LOOP_START_2JMP:    JMP LOOP_START_2
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Dynamic sprite routine
-; Programmed mainly by SMKDan, but based on some of my code
+; Programmed mainly by SMKDan
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-!TEMP = $09
+!Temp = $09
+!Timers = $0B
 
-!SLOTPTR = $0660		;16bit pointer for source GFX
-!SLOTBANK = $0662	;bank
-!SLOTDEST = $0663	;VRAM address
-!SLOTSUSED = $06FE	;how many slots have been used
+!SlotPointer = $0660|!Base2			;16bit pointer for source GFX
+!SlotBank = $0662|!Base2			;bank
+!SlotDestination = $0663|!Base2			;VRAM address
+!SlotsUsed = $06FE|!Base2			;how many slots have been used
 
-!MAXSLOTS = $04		;maximum selected slots
-
-SLOTS: db $CC,$C8,$C4,$C0	;avaliable slots.  Any more transfers and it's overflowing by a dangerous amount.
+!MAXSLOTS = $04			;maximum selected slots
 
 GETSLOT:
+get_dynamic_slot:
 	PHY		;preserve OAM index
 	PHA		;preserve frame
-	LDA !SLOTSUSED	;test if slotsused == maximum allowed
+	LDA !SlotsUsed	;test if slotsused == maximum allowed
 	CMP #!MAXSLOTS
-	BEQ NoneFree
-
-	PLA		;pop frame
-	REP #$20	;16bit A
-	AND.w #$00FF	;wipe high
-	XBA		;<< 8
-	LSR A		;>> 1 = << 7
-	STA !TEMP	;back to scratch
-	;;PEA SPRITEGFX	;push 16bit address, since i'm not sure how else i'm going to get the address of the label itself in TRASM
-	;;PLA		;pull 16bit address
-LDA.w #TapTapGFX&$FFFF
-	CLC
-	ADC !TEMP	;add frame offset	
-	STA !SLOTPTR	;store to pointer to be used at transfer time
-	SEP #$20	;8bit store
-	;;PHB		;push db...
-	;;PLA		;pull to A...
-LDA.b #TapTapGFX/$10000
-	STA !SLOTBANK	;store bank to 24bit pointer
-
-	LDY !SLOTSUSED	;calculate VRAM address + tile number
-	LDA.w SLOTS,y	;get tile# in VRAM
-	PHA		;preserve for eventual pull
-	SEC
-	SBC #$C0	;starts at C0h, they start at C0 in tilemap
-	REP #$20	;16bit math
-	AND.w #$00FF	;wipe high byte
-	ASL #5
-	CLC
-	ADC #$0B44	;add 0B44, base address of buffer	
-	STA !SLOTDEST	;destination address in the buffer
-
-	JSR DMABUFFER	;ROM -> RAM copy
-
-	SEP #$20	;8bit A	
-	INC !SLOTSUSED	;one extra slot has been used
-
-	PLA		;return starting tile number
-	PLY
-	RTS
+	BNE +
 		
-NoneFree:
 	PLA
 	PLY
 	LDA #$00	;zero on no free slots
 	RTS
 
++	PLA		;pop frame
+	REP #$20	;16bit A
+	AND.w #$00FF	;wipe high
+	XBA		;<< 8
+	LSR A		;>> 1 = << 7
+	STA !Temp	;back to scratch
+	LDA.w #gfx	;Get 16bit address
+	CLC
+	ADC !Temp	;add frame offset	
+	STA !SlotPointer	;store to pointer to be used at transfer time
+	SEP #$20	;8bit store
+	PHB : PLA
+	STA !SlotBank	;store bank to 24bit pointer
+
+	PHX		;This is how I made your boi a routine
+	LDX !SlotsUsed		;calculate VRAM address + tile number
+	LDA.L SlotsTable,X	;get tile# in VRAM
+	PLX
+	PHA		;preserve for eventual pull
+	SEC
+	SBC #$C0	;starts at C0h, they start at C0 in tilemap
+	REP #$20	;16bit math
+	AND.w #$00FF	;wipe high byte
+	ASL A		;multiply by 32, since 32 bytes/16 words equates to 1 32bytes tile
+	ASL A
+	ASL A
+	ASL A
+	ASL A
+if !SA1 == 1
+	CLC : ADC #$8000	;add 8000, base address of buffer
+else
+	CLC : ADC #$0B44	;add 0B44, base address of buffer	
+endif
+	STA !SlotDestination	;destination address in the buffer
+	SEP #$20
+	STZ !Timers
+	
+;;;;;;;;;;;;;;;;
+;Tansfer routine
+;;;;;;;;;;;;;;;;
+
 ;DMA ROM -> RAM ROUTINE
 
-DMABUFFER:
+if !SA1 == 1
 ;set destination RAM address
 	REP #$20
-	LDA !SLOTDEST
+	LDY #$C4
+	STY $2230
+	LDA.w !SlotDestination
+	STA $2235	;16bit RAM dest
+	        
+	         	;set 7F as bank
+
+;common DMA settings
+	         	;1 reg only
+	        	;to 2180, RAM write/read
+	         
+
+;first line
+	LDA !SlotPointer
+	STA $2232	;low 16bits
+	LDY !SlotBank
+	STY $2234	;bank
+	LDY #$80	;128 bytes
+	STZ $2238
+	STY $2238
+	LDY #$41
+	STY $2237
+	
+	LDY $318C
+	BEQ $FB
+	LDY #$00
+	STY $318C
+	STY $2230	;transfer
+
+;lines afterwards
+-	LDY #$C4
+	STY $2230
+	LDA.w !SlotDestination	;update buffer dest
+	CLC
+	ADC #$0200	;512 byte rule for sprites
+	STA !SlotDestination	;updated base
+	STA $2235	;updated RAM address
+
+	LDA !SlotPointer	;update source address
+	CLC
+	ADC #$0200	;512 bytes, next row
+	STA !SlotPointer
+	STA $2232	;low 16bits
+	LDY !SlotBank
+	STY $2234	;bank
+	LDY #$80
+	STZ $2238
+	STY $2238
+	LDY #$41
+	STY $2237
+	
+	LDY $318C
+	BEQ $FB
+	LDY #$00
+	STY $318C
+	STY $2230	;transfer
+	LDY !Timers
+	CPY #$02
+	BEQ +
+	INC !Timers
+	BRA -
++
+else
+;common DMA settings
+	REP #$20
+	STZ $4300	;1 reg only
+	LDY #$80	;to 2180, RAM write/read
+	STY $4301
+	
+;set destination RAM address
+	LDA !SlotDestination
 	STA $2181	;16bit RAM dest
 	LDY #$7F
 	STY $2183	;set 7F as bank
 
-;common DMA settings
-	STZ $4300	;1 reg only
-	LDY #$80	;to 2180, RAM write/read
-	STY $4301
-
-;first line
-	LDA !SLOTPTR
+	LDA !SlotPointer
 	STA $4302	;low 16bits
-	LDY !SLOTBANK
+	LDY !SlotBank
 	STY $4304	;bank
 	LDY #$80	;128 bytes
 	STY $4305
@@ -1435,63 +1500,40 @@ DMABUFFER:
 	STY $420B	;transfer
 
 ;second line
-	LDA !SLOTDEST	;update buffer dest
+-	LDA !SlotDestination	;update buffer dest
 	CLC
 	ADC #$0200	;512 byte rule for sprites
-	STA !SLOTDEST	;updated base
+	STA !SlotDestination	;updated base
 	STA $2181	;updated RAM address
 
-	LDA !SLOTPTR	;update source address
+	LDA !SlotPointer	;update source address
 	CLC
 	ADC #$0200	;512 bytes, next row
-	STA !SLOTPTR
+	STA !SlotPointer
 	STA $4302	;low 16bits
-	LDY !SLOTBANK
+	LDY !SlotBank
 	STY $4304	;bank
 	LDY #$80
 	STY $4305
 	LDY #$01
 	STY $420B	;transfer
+	LDY !Timers
+	CPY #$02
+	BEQ +
+	INC !Timers
+	BRA -
++
+endif
+	
+	SEP #$20	;8bit A	
+	INC !SlotsUsed	;one extra slot has been used
 
-;third line
-	LDA !SLOTDEST	;update buffer dest
-	CLC
-	ADC #$0200	;512 byte rule for sprites
-	STA !SLOTDEST	;updated base
-	STA $2181	;updated RAM address
+	PLA		;return starting tile number
+	PLY
+	RTS
 
-	LDA !SLOTPTR	;update
-	CLC
-	ADC #$0200
-	STA !SLOTPTR
-	STA $4302
-	LDY !SLOTBANK
-	STY $4304
-	LDY #$80
-	STY $4305
-	LDY #$01
-	STY $420B	;transfer
+SlotsTable:			;avaliable slots.  Any more transfers and it's overflowing by a dangerous amount.
+	db $CC,$C8,$C4,$C0		
 
-;fourth line
-	LDA !SLOTDEST	;update buffer dest
-	CLC
-	ADC #$0200	;512 byte rule for sprites
-	STA !SLOTDEST	;updated base
-	STA $2181	;updated RAM address
 
-	LDA !SLOTPTR
-	CLC
-	ADC #$0200
-	STA !SLOTPTR
-	STA $4302
-	LDY !SLOTBANK
-	STY $4304
-	LDY #$80
-	STY $4305
-	LDY #$01
-	STY $420B
-
-	SEP #$20	;8bit A
-	RTS		;all done, return
-
-incbin taptap.bin -> TapTapGFX
+incbin taptap.bin -> gfx
